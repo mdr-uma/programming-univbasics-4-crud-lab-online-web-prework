bd4a57074e13aab289cacacf28a64207830ea66e
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-27 20:31:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,40 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  ["red", "blue", "green", "pink"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array.push("wow")
+  p add_element_to_end_of_array(array, element)
+
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
